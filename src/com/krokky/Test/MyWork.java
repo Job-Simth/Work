bb69fc6e98498a74c5001d01f326e9653c0003cc
krokky <875594067@qq.com>
2018-05-23 16:31:10 +0800

    拷贝文件

--- src/com/krokky/Test/MyWork.java
+++ src/com/krokky/Test/MyWork.java
@@ -2,20 +2,17 @@ package com.krokky.Test;
 
 import java.io.*;
 
-public class test {
+public class MyWork {
     public static void main(String[] args) {
-        FileWriter fw ;
-        BufferedReader br ;
-
         try {
-            fw = new FileWriter("e:\\test.txt");
-            br = new BufferedReader(new InputStreamReader(new FileInputStream("e:\\b.txt"),"GBK"));
-            String str;
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("e:\\b.txt"),"GBK"));
+            FileWriter fw = new FileWriter("e:\\test1.txt");
+            String str ;
             while ((str=br.readLine())!=null){
                 fw.write(str+"\r\n");
                 fw.flush();
             }
-            br.close();
+            fw.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
