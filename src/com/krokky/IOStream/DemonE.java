718889ac0f0b2342c607585e94a72966ee52b0ad
krokky <875594067@qq.com>
2018-05-23 16:29:48 +0800

    文件读取

---
+++ src/com/krokky/IOStream/DemonE.java
@@ -0,0 +1,25 @@
+package com.krokky.IOStream;
+
+import java.io.*;
+
+public class DemonE {
+    public static void main(String[] args) {
+        String str = "abcdefg";
+        byte[] b = str.getBytes();
+
+        try {
+            File file = new File("E://d.txt");
+            OutputStream out = new FileOutputStream(file);
+
+            BufferedOutputStream bos = new BufferedOutputStream(out);
+            //b:写的内容,off:从第几开始,len:写几个字节
+            bos.write(b, 0, str.length()*2);
+            bos.flush();
+            bos.close();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
