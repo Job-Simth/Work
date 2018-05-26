4e980a1f621f7769bb9b576f606cf9774df93450
krokky <875594067@qq.com>
2018-05-26 19:58:11 +0800

    使用原地算法压缩字符串

--- src/com/krokky/leetcode_Practice/ZipWord.java
+++ src/com/krokky/leetcode_Practice/ZipWord.java
@@ -1,45 +1,55 @@
 package com.krokky.leetcode_Practice;
 
-import java.util.Arrays;
-
 public class ZipWord {
     public static void main(String[] args) {
-        char[] c = {'a', 'a', 'a', 'b', 'b', 'c', 'c', 'd'};
-        int length = c.length - 1;
+        char[] chars = {'a'};
+        int i = new Solution8().compress(chars);
+        for (int j = 0; j < i; j++) {
+            System.out.print(chars[j]+" ");
+        }
+    }
+}
+
+class Solution8 {
+    public int compress(char[] chars) {
         int count = 1;
         int temp = 0;
-        for (int i = 0; i < length - 1; ++i) {
-            if (c[i] == c[i + 1]) {
+        for (int i = 0; i <chars.length; ++i) {
+            if (i + 1 == chars.length) {
+                temp = Write(chars, count, temp, i);
+                count = 1;
+            } else if (chars[i] == chars[i + 1]) {
                 ++count;
             } else {
-                temp = Write(c, count, temp, i);
+                temp = Write(chars, count, temp, i);
                 count = 1;
             }
         }
-        System.out.println(temp);
-        System.out.println(Arrays.toString(c));
+        return temp;
     }
 
-    private static int Write(char[] c, int count, int temp, int i) {
+
+    private static int Write(char[] chars, int count, int temp, int i) {
         final int IntToChar = 48;
-        c[temp] = c[i];
-        temp++;
-        if (count > 1) {
-            if (count < 10) c[temp] = (char) (count + IntToChar);
-            else if (count < 100) {
-                c[temp] = (char) (count / 10 + IntToChar);
-                c[++temp] = (char) (count % 10 + IntToChar);
-            } else if (count < 1000) {
-                c[temp] = (char) (count / 100 + IntToChar);
-                c[++temp] = (char) (count / 10 % 10 + IntToChar);
-                c[++temp] = (char) (count / 100 % 10 + IntToChar);
-            } else if (count == 1000) {
-                c[temp] = (char) (1 + IntToChar);
-                c[++temp] = (char) (0 + IntToChar);
-                c[++temp] = (char) (0 + IntToChar);
-                c[++temp] = (char) (0 + IntToChar);
-            }
+        chars[temp] = chars[i];
+        ++temp;
+        if (count == 1) {
+            return temp;
+        } else if (count > 1 && count < 10) chars[temp] = (char) (count + IntToChar);
+        else if (count < 100) {
+            chars[temp] = (char) (count / 10 + IntToChar);
+            chars[++temp] = (char) (count % 10 + IntToChar);
+        } else if (count < 1000) {
+            chars[temp] = (char) (count / 100 + IntToChar);
+            chars[++temp] = (char) (count / 10 % 10 + IntToChar);
+            chars[++temp] = (char) (count / 100 % 10 + IntToChar);
+        } else if (count == 1000) {
+            chars[temp] = (char) (1 + IntToChar);
+            chars[++temp] = (char) (0 + IntToChar);
+            chars[++temp] = (char) (0 + IntToChar);
+            chars[++temp] = (char) (0 + IntToChar);
         }
         return ++temp;
     }
 }
+
